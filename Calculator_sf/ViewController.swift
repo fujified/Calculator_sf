9d8a4f588c43a026544f882eed2850e1d9ac0a53
likeaspock <uktras@gmail.com>
2020-12-07 14:46:03 +0300

    Initial Commit

---
+++ Calculator_sf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_sf
+//
+//  Created by mr spock on 12/7/20.
+//  Copyright © 2020 mr spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
